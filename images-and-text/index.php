7f2617d9cb5c8d210e7986e69e4ba45c7949abc4
Stephen Braitsch <yourname@yourdomain.com>
2012-04-04 15:09:15 -0700

    documenting displaying images.

--- images-and-text/index.php
+++ images-and-text/index.php
@@ -4,10 +4,33 @@
         <div id='content'>
             <div id="header">
                 <h1>Images & Text</h1>
-                <p>coming soon</p>
+                <p>JS3 has built in support to easily display Images and render Text.</p>
                 <hr>
             </div>
-        </div>
+            
+        <h2>Displaying Images</h2>
+        <p>To display an Image on the canvas, simply create a JS3Image Object and pass in the path of the file to be loaded.</p>
+            <pre><code class='javascript'>
+        var stage = new JS3('my-canvas');                
+        var img = new JS3Image( {src:'nyancat.png'} );
+            img.x = 25; 
+            img.y = 25;                          
+        stage.addChild(img);
+            </code></pre>
+        <canvas id="img-1" width='898' height='100'></canvas>
+        <p>Or of course you can specify the <u><b>src</b></u> on the JS3Image itself instead of passing it into the constructor.</p>            
+            <pre><code class='javascript'>
+        var stage = new JS3('my-canvas');                
+        var img = new JS3Image( {x:25, y:25} );
+            img.src = 'nyancat.png';
+            img.rotation = 90;
+        stage.addChild(img);
+            </code></pre>
+        <canvas id="img-2" width='898' height='100'></canvas>
+        <p><b>Supported Image file formats include <span style='color:blue'>png, jpg, </span>and <span style='color:blue'>gif.</span></b></p><hr>
+        <h2>Rendering Text</h2>
+       
+        <?php include ('../local/footer.php'); ?>        
         <script type="text/javascript" src="./images-and-text.js"></script>            
 	    <script type="text/javascript" src="http://yandex.st/highlightjs/6.1/highlight.min.js"></script>
         <script>hljs.initHighlightingOnLoad();</script>
