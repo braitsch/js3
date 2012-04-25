b6452b7c93aceb09099447157eec40859352a9f4
Stephen Braitsch <stephen@quietless.com>
2012-04-25 10:52:12 -0700

    adding link to github repository.

--- local/header.php
+++ local/header.php
@@ -74,4 +74,6 @@ function linkto($n){ global $root; echo $root.$n;}
             </div><!-- navbar-inner -->          
             
         </div><!-- navbar -->
+        
+<a href="http://github.com/braitsch/js3"><img style="position:absolute;top:41px;right:0;border:0;" src="https://a248.e.akamai.net/camo.github.com/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub"></a>
             
